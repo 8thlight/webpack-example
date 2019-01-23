ba51ef7199551f3c94784da8648c6dff2933ba0a
Eric Smith <esmith@8thlight.com>
2019-01-23 11:12:30 -0600

    Simple index.html with a source file
    This is just index, source file, nothing else. Works!

---
+++ src/index.js
@@ -0,0 +1,10 @@
+function component() {
+    let element = document.createElement('div');
+
+    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+
+    return element;
+}
+
+document.body.appendChild(component());
+console.log('I got this');
