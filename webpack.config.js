fe2218d0646eb650a1a3bdc62e31bb1f91175518
Eric Smith <esmith@8thlight.com>
2019-01-24 01:15:18 -0600

    Add a development server
    This refreshes with sourcemaps.

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,8 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 module.exports = {
+    mode: 'development',
+    devtool: 'inline-source-map',
     entry: './src/index.js',
     plugins: [
         new CleanWebpackPlugin(['dist']),
@@ -10,6 +12,9 @@ module.exports = {
             title: 'Development Server'
         })
     ],
+    devServer: {
+        contentBase: './dist'
+    },
     output: {
         filename: '[name].bundle.js',
         path: path.resolve(__dirname, 'dist')
